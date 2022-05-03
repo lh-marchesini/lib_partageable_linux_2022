5b977df48fe68e08f34a4a1115fc291030ac7285
lh-marchesini <103187019+lh-marchesini@users.noreply.github.com>
2022-05-03 17:50:48 +0200

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -42,10 +42,10 @@ int main(int argc, char ** argv)
 	/* invoke function, passing value of integer as a parameter */
 	//(*fptr)(*iptr);
 
-	//valeur1=composant1(data1,data2);
+	//valeur1=composant1("3,5");
 	valeur1=(*fptr1)(data1,data2);
 
-	//valeur2=composant2(data1,data2);
+	//valeur2=composant2("3,5");
 	valeur2=(*fptr2)(data1,data2);
 
 	std::cout << getComposant1Version() << std::endl;
